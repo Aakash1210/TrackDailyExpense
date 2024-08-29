e143df6ea796bc7dada664017012060dc8ac6768
Aakash Chauhan <Aakash1210>
2024-08-29 23:25:04 +0530

    adding trycatch and features

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
-from dailyexpense.views import home,ExpenseAPI,CategoryAPI
+from dailyexpense.views import home,ExpenseAPI,CategoryAPI,get_daily_expense_by_id
 from django.urls import path
 
 urlpatterns = [
     path('', home),
-    path('daily_expense/',ExpenseAPI.as_view(),name='daily_expense'),
-    path('categories/', CategoryAPI.as_view(), name='get_category'),
+    path('daily_expense/',ExpenseAPI.as_view(),name='all_daily_expense'),
+    path('categories/', CategoryAPI.as_view(), name='all_category'),
+    path('daily_expense/id/<int:pk>/',get_daily_expense_by_id,name='daily_expense_id'),
+    path('daily_expense/delete/<int:pk>/',ExpenseAPI.as_view(),name='daily_expense_del_id')
 
 ]
--- dailyexpense/views.py
+++ dailyexpense/views.py
@@ -5,67 +5,110 @@ from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import DailyExpenseSerializer,CategorySerializer
 from .models import DailyExpense,Category
+from django.db import IntegrityError
+from rest_framework.exceptions import ValidationError
+
 # Create your views here.
 
 @api_view(['GET'])
 def home(request):
     return Response({"status":True,"message":"Hello,World!!"},status.HTTP_200_OK)
 
+
+@api_view(['GET'])
+def get_daily_expense_by_id(request,pk):
+    try:
+        daily_expense_id = DailyExpense.objects.get(pk=pk)
+    except DailyExpense.DoesNotExist:
+        return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = DailyExpenseSerializer(daily_expense_id)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 class ExpenseAPI(APIView):
     def get(self,request):#DailyExpense
         try:
             daily_expense_data=DailyExpense.objects.all()
-            serializer=DailyExpenseSerializer(data=daily_expense_data, many=True)
+        except DailyExpense.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer=DailyExpenseSerializer(daily_expense_data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self,request):
+        try:
+            serializer=DailyExpenseSerializer(data=request.data,many=True)
             if serializer.is_valid():
                 serializer.save()
-            return Response(serializer.data)
-        except Exception as e:
-            return Response({
-                'status':False,
-                'message':serializer.errors
-            })
+                return Response(serializer.data, status=status.HTTP_201_CREATED) 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except IntegrityError:
+            return Response({"error": "Database integrity error occurred."}, status=status.HTTP_400_BAD_REQUEST)
         
-    def post(self,request):
-        daily_expense_data=request.data
-        serializer=DailyExpenseSerializer(data=daily_expense_data,many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response({
-                'status':serializer.data,
-                'message':serializer.errors
-            })
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+        except Exception as e:
+            return Response({"error": "An unexpected error occurred.","message":{e}}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def put(self,request):
         pass
 
     def patch(self,request):
-        daily_expense_data=request.data
-        daily_expense_objs=DailyExpense.objects.get(id=daily_expense_data['id'])
-        serializer=DailyExpenseSerializer(daily_expense_objs,data=daily_expense_data,partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+        try:
+            daily_expense_data=request.data
+            try:
+                daily_expense_objs=DailyExpense.objects.get(id=daily_expense_data['id'])
+            except DailyExpense.DoesNotExist:
+                return Response({"error": "Daily expense not found."}, status=status.HTTP_404_NOT_FOUND)
+            
+            serializer=DailyExpenseSerializer(daily_expense_objs,data=daily_expense_data,partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except IntegrityError:
+            return Response({"error": "Database integrity error occurred."}, status=status.HTTP_400_BAD_REQUEST)
+
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return Response({"error": "An unexpected error occurred.","message":{e}}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    def delete(self,request):
-        daily_expense_data=request.data
-        daily_expense_objs=DailyExpense.objects.get(id=daily_expense_data['id'])
-        daily_expense_objs.delete()
-        return Response({"message":f"Delete the data"})
+    def delete(self,request,pk):
+        try:
+            daily_expense_id = DailyExpense.objects.get(pk=pk)
+        except DailyExpense.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+        daily_expense_id.delete()
+        return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 
 class CategoryAPI(APIView):
     def get(self,request):#Category
-        category_data=Category.objects.all()
-        serializer=CategorySerializer(data=category_data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
+        try:
+            category_data=Category.objects.all()
+        except Category.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer=CategorySerializer(category_data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
     def post(self,request):
-        category_data=request.data
-        serializer=CategorySerializer(data=category_data,many=True)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
+        try:
+            category_data=request.data
+            serializer=CategorySerializer(data=category_data,many=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED) 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except IntegrityError:
+            return Response({"error": "Database integrity error occurred."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+        except Exception as e:
+            return Response({"error": "An unexpected error occurred.","message":{e}}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
